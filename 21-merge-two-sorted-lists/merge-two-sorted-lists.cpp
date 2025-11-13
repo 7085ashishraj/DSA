3962ff85076c18b064caba1c28e6c22fe9af4b73
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2025-11-13 15:21:45 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (27.78%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -12,31 +12,52 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         //solution using extra space
-        ListNode* tempA = list1;
-        ListNode* tempB = list2;
-        //created a new node
-        ListNode* c = new ListNode(100);
-        ListNode* tempC = c;
+        // ListNode* tempA = list1;
+        // ListNode* tempB = list2;
+        // //created a new node
+        // ListNode* c = new ListNode(100);
+        // ListNode* tempC = c;
 
-        while(tempA!=nullptr && tempB!=nullptr){
-            if(tempA->val <= tempB->val){
-                ListNode* t = new ListNode(tempA->val);
-                tempC->next = t;
-                tempC = t;
-                tempA = tempA->next;
+        // while(tempA!=nullptr && tempB!=nullptr){
+        //     if(tempA->val <= tempB->val){
+        //         ListNode* t = new ListNode(tempA->val);
+        //         tempC->next = t;
+        //         tempC = t;
+        //         tempA = tempA->next;
+        //     }
+        //     else{
+        //         ListNode* t = new ListNode(tempB->val);
+        //         tempC->next = t;
+        //         tempC = t;
+        //         tempB = tempB->next;
+        //     }
+        // }
+        // if(tempA == nullptr){
+        //     tempC->next = tempB;
+        // }else{
+        //     tempC->next = tempA;
+        // }
+        // return c->next;
+
+        //In O(1) space:-
+
+        ListNode* c = new ListNode(100);
+        ListNode* t = c;
+
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val <= list2->val){
+                t->next = list1;
+                list1 = list1->next;
+                t = t->next;
             }
             else{
-                ListNode* t = new ListNode(tempB->val);
-                tempC->next = t;
-                tempC = t;
-                tempB = tempB->next;
+                t->next = list2;
+                list2 = list2->next;
+                t = t->next;
             }
         }
-        if(tempA == nullptr){
-            tempC->next = tempB;
-        }else{
-            tempC->next = tempA;
-        }
+        if(list1==nullptr) t->next = list2;
+        else t->next = list1;
         return c->next;
     }
 };
