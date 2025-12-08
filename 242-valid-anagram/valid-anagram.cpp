f1dad819ef7d289eb903dcb9c4f7274937953f33
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2025-12-08 21:18:20 +0530

    Time: 7 ms (20.64%) | Memory: 9.7 MB (47.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
+        map<int, int> mp1, mp2;
 
-        unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
-
-        for(int i=0;i<s.length();i++){
-            mp1[s[i]]++;
+        for(auto &ch : s){
+            mp1[ch]++;
         }
 
-        for(int i=0;i<t.length();i++){
-            mp2[t[i]]++;
+        for(auto &ch : t){
+            mp2[ch]++;
         }
 
-        for(auto x: mp1){
-            char ch1 = x.first;
-            int freq1 = x.second;
-            if(mp2.find(ch1)!=mp2.end()){
-                int freq2 = mp2[ch1];
-                if(freq1 != freq2) return false;
-                
-            }
-            else return false;
+       for(char ch = 'a'; ch<='z'; ch++){
+        if(mp1[ch] != mp2[ch]){
+            return false;
         }
-
-       
+       }
         return true;
     }
 };
