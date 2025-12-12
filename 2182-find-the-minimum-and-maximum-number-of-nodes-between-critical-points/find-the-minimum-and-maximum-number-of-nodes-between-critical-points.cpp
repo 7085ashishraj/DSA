c73dff309a0d171cf7ea4c4704cd5ccb77926184
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2025-12-12 12:01:17 +0530

    Time: 12 ms (10.21%) | Memory: 124.5 MB (78.90%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,83 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        //starting from 1 coz it's sure that first and last will not be critical point
+        int idx = 1;
+
+        //creating 3 node to compare each node
+        ListNode* a = head;
+        ListNode* b = head->next;
+        if(!b) return {-1,-1};
+        ListNode* c = head->next->next;
+
+        //two indexes to find the max distance
+        int f_idx = -1;
+        int s_idx = -1;
+
+        //comparing
+        if(c==NULL) return {-1,-1};
+
+        while(c != NULL){
+            if((b->val > a->val && b->val > c->val) || (b->val < a->val && b->val < c->val)){
+                if(f_idx == -1) f_idx = idx; //first critical point
+                 s_idx = idx; //last critical point
+               
+            }
+            //incrementing indxes
+
+            a = a->next;
+            b = b->next;
+            c = c->next;
+            idx++;
+        }
+
+        //If less than 2 critical points found
+        if(s_idx == -1 || f_idx == s_idx) return {-1,-1};
+        //calculate max distance
+        int max_dis = s_idx - f_idx;
+
+        //MINIMUM DISTANCE
+
+        int min_dis = INT_MAX;
+
+        //Reinitializing
+        f_idx = -1;
+        s_idx = -1;
+
+        idx = 1;
+
+        a = head;
+        b = head->next;
+        c = head->next->next;
+
+        //comparing
+        while(c != NULL){
+            if((b->val > a->val && b->val > c->val) || b->val < a->val && b->val < c->val){
+               f_idx = s_idx;
+               s_idx = idx;
+
+               if(f_idx != -1){
+                int d = s_idx - f_idx;
+                min_dis = min(min_dis, d); 
+               }
+            }
+            //incrementing indxes
+
+            a = a->next;
+            b = b->next;
+            c = c->next;
+            idx++;
+        }
+        return{min_dis, max_dis};
+    }
+};
