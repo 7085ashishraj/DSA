206fcca49479d6bd2e5cb37a75f7672b8921faf1
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2026-01-23 13:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (24.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pre_pdt(n);
-        vector<int> suff_pdt(n);
+
+        vector<int> pPdt(n);
+        vector<int>sPdt(n);
         vector<int> ans(n);
+        
+        pPdt[0] = 1;
+        sPdt[n-1] = 1;
 
-        pre_pdt[0] = 1;
-        suff_pdt[n-1] = 1;
-
-        for(int i=1;i<n;i++){
-           pre_pdt[i] = pre_pdt[i-1] * nums[i-1];
+        for(int i=1;i<nums.size();i++){
+            pPdt[i] = pPdt[i-1] * nums[i-1];
         }
 
         for(int i=n-2;i>=0;i--){
-           suff_pdt[i] = suff_pdt[i+1] * nums[i+1];
+            sPdt[i] = sPdt[i+1] * nums[i+1];
         }
-        for(int i=0;i<n;i++){
-            ans[i] = pre_pdt[i]*suff_pdt[i];
+
+        for(int i=0;i<nums.size();i++){
+            ans[i] = pPdt[i] * sPdt[i];
         }
+
         return ans;
+
     }
 };
