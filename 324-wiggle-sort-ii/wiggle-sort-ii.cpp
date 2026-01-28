824a53ce4b9c6520f450c2a3b9c3f993eb4ba38f
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2026-01-28 11:17:30 +0530

    Time: 2 ms (53.28%) | Memory: 21.9 MB (44.89%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -3,21 +3,16 @@ public:
     void wiggleSort(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
+        int mid = (n-1)/2;
+        int last = n-1;
         vector<int> res(n);
-        int curr_idx = n-1;
-
-        for(int i=1;i<n;i+=2){
-            res[i] = nums[curr_idx];
-            curr_idx--;
-
-        }
-
-        for(int i=0;i<n;i+=2){
-            res[i] = nums[curr_idx];
-            curr_idx--;
-
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                res[i] = nums[mid--];
+            }else{
+                res[i] = nums[last--];
+            }
         }
         nums = res;
-        
     }
 };
