749fb10b6e7102c9e115ad9b5f3bc568ee181af1
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2026-02-09 10:50:44 +0530

    Time: 44 ms (55.52%) | Memory: 45.5 MB (54.95%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        //storing for sum and frequency(key ->value)
+        unordered_map<int, int> mp;
+        int sum = 0;
+        mp[0] = 1; //can be edge case
+        int cnt = 0;
+        for(int x : arr){
+            sum+=x;
+            if(mp.find(sum - k) != mp.end()){
+                cnt+=mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
