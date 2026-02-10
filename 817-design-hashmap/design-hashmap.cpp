15f619884c4b615562d9b82be53353d0a7134749
7085ashishraj <145831668+7085ashishraj@users.noreply.github.com>
2026-02-10 10:04:46 +0530

    Time: 218 ms (5.02%) | Memory: 311.7 MB (5.26%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -4,7 +4,9 @@ class MyHashMap {
     int MaxSize = 1e6 + 1;
 public:
     MyHashMap() {
-        ans.resize(MaxSize, -1);
+        for(int i=0;i<MaxSize;i++){
+            ans.push_back(-1);
+        }
     }
     
     void put(int key, int value) {
