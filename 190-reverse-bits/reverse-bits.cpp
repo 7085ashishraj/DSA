7f9bfd0e01dd0bf24b0524f264e2cc25939168e8
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-02-16 20:13:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.07%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        if(n==0) return 0;
+        if(n==0) return 0; //base case
         int result = 0;
         for(int i=1;i<=32;i++){
-            result <<= 1;
-            result = result | (n &1);
-            n>>=1;
+            result <<= 1; //result me ek left shift karke make place, so that we can put a lsb from n
+            result = result | (n &1); //append in result
+            n>>=1; 
         }
         return result;
     }
