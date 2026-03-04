98815a97c06a64e0d34b894b3bebe14208146a7e
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-03-04 12:37:03 +0530

    Time: 3 ms (19.50%) | Memory: 16.6 MB (73.70%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool rowCheck(int row, vector<vector<int>>& mat){
-        int cnt = 0 ;
+    bool rowCheck(int row, vector<vector<int>> &mat){
+        int cnt = 0;
         for(int i=0;i<mat[0].size();i++){
             if(mat[row][i]==1) cnt++;
         }
         return cnt==1;
     }
 
-     bool columnCheck(int col, vector<vector<int>>& mat){
+    bool colCheck(int col, vector<vector<int>> &mat){
         int cnt = 0;
         for(int i=0;i<mat.size();i++){
-            if(mat[i][col]==1) cnt++;
+            if(mat[i][col] == 1) cnt++;
         }
         return cnt==1;
     }
-
-    
     int numSpecial(vector<vector<int>>& mat) {
         int cnt = 0;
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
                 if(mat[i][j]==0) continue;
                 else{
-                    if(rowCheck(i,mat) && columnCheck(j,mat)) cnt++;
+                    if(rowCheck(i,mat) && colCheck(j,mat)) cnt++;
                 }
             }
         }
