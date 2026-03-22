beafb322870f6144a9de5ba292ad2b9efd8a7daa
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-03-22 22:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (41.55%) - LeetSync

---
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat){
+        int n = mat.size();
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+        //reverse
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+
+    }
+
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        //simply take transpose and reverse
+       for (int k = 0; k < 4; k++) { //4 times coz square mat can be obtained using 4 rotations
+            if (mat == target) return true;
+            rotate(mat);
+        }
+        return false;
+
+    }
+};
