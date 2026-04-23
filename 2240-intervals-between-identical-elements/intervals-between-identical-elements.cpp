12cdc2dfaf0842fbb4654c4f9df34e58aeaf7571
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-04-23 13:14:02 +0530

    Time: 167 ms (39.22%) | Memory: 142.6 MB (49.12%) - LeetSync

---
+++ 2240-intervals-between-identical-elements/intervals-between-identical-elements.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    typedef long long ll;
+    vector<long long> getDistances(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<ll> res(n, 0);
+
+        unordered_map<int, ll> indexSum; //nums[i]->sum of i's
+        unordered_map<int, ll> indexCount; // nums[i]->freq
+
+        //left to right
+        for(int i=0; i<n; i++){
+            ll freq = indexCount[nums[i]];
+            ll sum =  indexSum[nums[i]];
+
+            res[i] += freq * i - sum;
+            indexCount[nums[i]] += 1;
+            indexSum[nums[i]] += i;
+        }
+        indexSum.clear();
+        indexCount.clear();
+
+        //Right to left
+        for(int i=n-1; i>=0; i--){
+            ll freq = indexCount[nums[i]];
+            ll sum =  indexSum[nums[i]];
+
+            res[i] += sum - freq*i;
+            indexCount[nums[i]] += 1;
+            indexSum[nums[i]] += i;
+        }
+        return res;
+        
+    }
+};
