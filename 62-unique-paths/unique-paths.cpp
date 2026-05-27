f39d1eecaad059714f73035513dce9a1d1d8c415
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-05-27 13:45:46 +0530

    Time: 2 ms (20.64%) | Memory: 9.5 MB (28.29%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int cntPath(int i, int j, vector<vector<int>> &dp){
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        int left = cntPath(i,j-1,dp);
+        int up = cntPath(i-1, j, dp);
+
+        return dp[i][j] = left + up;
+    }
+    int uniquePaths(int m, int n) {
+        //memoization
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return cntPath(m-1, n-1, dp);
+    }
+};
