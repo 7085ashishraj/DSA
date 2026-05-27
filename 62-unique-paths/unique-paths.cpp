1bfed19dd53316b43b64578163ed4aa5b8fdfdc1
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-05-27 14:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.29%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int cntPath(int i, int j, vector<vector<int>> &dp){
-        if(i<0 || j<0) return 0;
-        if(i==0 && j==0) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = cntPath(i,j-1,dp);
-        int up = cntPath(i-1, j, dp);
+    // int cntPath(int i, int j, vector<vector<int>> &dp){
+    //     if(i<0 || j<0) return 0;
+    //     if(i==0 && j==0) return 1;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int left = cntPath(i,j-1,dp);
+    //     int up = cntPath(i-1, j, dp);
 
-        return dp[i][j] = left + up;
-    }
+    //     return dp[i][j] = left + up;
+    // }
     int uniquePaths(int m, int n) {
-        //memoization
+        //tabulation
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return cntPath(m-1, n-1, dp);
+        //fill 0th row and 0th column with 1
+        //0th col
+        for(int i=0;i<m;i++){
+            dp[i][0] = 1;
+        }
+        //0th row
+        for(int i=0;i<n;i++){
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int left = dp[i][j-1];
+                int up = dp[i-1][j];
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
