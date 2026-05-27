a9b2413e7af6d7cf2b703039fca06f31ff66ba80
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-05-27 10:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int climb(int n, vector<int> &dp){
-        if(n==1 || n==2) return n;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = climb(n-1,dp) + climb(n-2, dp);
-    }
     int climbStairs(int n) {
-        
-        //memoization
+        //tabulation
         vector<int> dp(n+1, -1);
-        //dp[i] -> no of ways to reach i
-        return climb(n, dp);
-        
+        if(n==1 || n==2) return n;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
