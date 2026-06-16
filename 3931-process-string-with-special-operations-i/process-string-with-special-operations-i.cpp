5b2598fb20b0252d589b2e9d2950cbc5dae3b0b5
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-06-16 20:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n = s.length();
+        string result = "";
+        for(auto& ch : s ){
+            if(ch == '*'){
+                if(result.size() != 0){
+                    result.pop_back();
+                }
+            }
+            else if(ch == '#') result += result;
+            else if(ch == '%') reverse(result.begin(), result.end());
+
+            else result += ch;
+        }
+        return result;
+    }
+};
