046b69e78c30b137b0852338d7d49fe14dc612ca
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-07-07 13:57:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (42.26%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    typedef long long ll;
+    ll sumAndMultiply(int n) {
+        ll x = 0;
+        ll sum = 0;
+        for(auto& ch : to_string(n)){
+            if(ch != '0'){
+                sum += (ch - '0');
+                x = x *10 + (ch - '0'); 
+            }
+        }
+        return x * sum;
+        
+    }
+};
