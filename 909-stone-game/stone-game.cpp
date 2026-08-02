ce4a73fb40222c7b5d633280ec2ec47561fe24d7
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-08-02 11:30:35 +0530

    Time: 7 ms (35.23%) | Memory: 11.7 MB (34.97%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int t[501][501];
+    int solve(int i, int j, vector<int> &piles){
+        if(i > j) return 0;
+        if(i == j) return piles[i];
+
+        if(t[i][j] != -1) return t[i][j];
+
+        int take_i = piles[i] + min(solve(i+2,j,piles), solve(i+1, j-1, piles));
+        int take_j = piles[j] + min(solve(i+1,j-1,piles), solve(i, j-2, piles));
+
+        return t[i][j] =  max(take_i, take_j);
+
+    }
+    bool stoneGame(vector<int>& piles) {
+        memset(t, -1, sizeof(t));
+        int n = piles.size();
+        int total_score = accumulate(begin(piles), end(piles), 0);
+
+        int p1_score = solve(0, n-1, piles);
+        int p2_score = total_score - p1_score;
+
+        if(p1_score > p2_score) return true;
+        else return false;
+
+        
+    }
+};
