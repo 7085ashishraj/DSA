6eb54e03d43932e27477ea907b379bc813aca3fe
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-08-09 20:02:44 +0530

    Time: 8 ms (65.33%) | Memory: 11.1 MB (81.19%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
-public:
-    
+public: 
     int n;
     int t[2][101][101]; 
     int solveForAlice(vector<int>& piles, int person, int i, int M) {
-        
-        if(i >= n)
-            return 0;
-        
-        if(t[person][i][M] != -1)
-            return t[person][i][M];
-        
+        if(i >= n) return 0;
+             
+        if(t[person][i][M] != -1) return t[person][i][M];
+            
         int result = (person == 1) ? -1 : INT_MAX;
         
         int stones = 0;
@@ -23,19 +19,15 @@ public:
                 result = max(result, stones + solveForAlice(piles, 0, i+x, max(M, x)));
             } else { //Bob
                 result = min(result, solveForAlice(piles, 1, i+x, max(M, x)));
-            }
-            
+            }   
         }
-        
         return t[person][i][M] = result;
         
     }
     
     int stoneGameII(vector<int>& piles) {
-        n = piles.size();
-        
+        n = piles.size(); 
         memset(t, -1, sizeof(t));
-        
         return solveForAlice(piles, 1, 0, 1);
     }
 };
