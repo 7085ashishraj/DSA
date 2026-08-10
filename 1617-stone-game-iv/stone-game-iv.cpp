d933b14ddc4d1164245d668e94bb4ce5fcb91d3d
Ashish Raj <145831668+7085ashishraj@users.noreply.github.com>
2026-08-10 22:28:33 +0530

    Time: 44 ms (47.57%) | Memory: 16.6 MB (45.73%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> t;
+
+    bool solve(int n) {
+        if(n == 0) {
+            return false;
+        }
+        if(t[n] != -1) {
+            return t[n] == 1 ? true : false;
+        }
+        for(int k = 1; k*k <= n; k++) { 
+            if(solve(n - (k*k)) == false) {//Call for Bob //False - Bob lost it
+                //Alice won the game
+                return t[n] = true;
+            }
+
+        }
+        return t[n] = false; //Alice could never win. Lost it.
+    }
+
+    bool winnerSquareGame(int n) {
+        t.assign(n+1, -1);
+        return solve(n); //Alice k lie call hai ye. If it's true, Alice wins, else Alice looses
+    }
+};
